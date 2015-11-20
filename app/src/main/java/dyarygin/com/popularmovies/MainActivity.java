82a8231d1e0d70632f9633cc7a7b93be8255c670
deemoun <deemounus@gmail.com>
2015-11-19 18:03:37 -0800

    Instead of using XML, programmatically start activity inside a MainActivity

--- app/src/main/java/dyarygin/com/popularmovies/MainActivity.java
+++ app/src/main/java/dyarygin/com/popularmovies/MainActivity.java
@@ -1,7 +1,8 @@
 package dyarygin.com.popularmovies;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.support.v7.app.AppCompatActivity;
 
 import com.facebook.stetho.Stetho;
@@ -11,20 +12,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-//        FragmentManager fm = getSupportFragmentManager();
-//        Fragment fragment = fm.findFragmentById(R.layout.fragment_main);
-//        if (fragment == null) {
-//            fragment = new MainActivityFragment();
-//            fm.beginTransaction()
-//                    .add(R.id.frame_container, fragment)
-//                    .commit();
-//        }
+        setContentView(R.layout.fragment_holder);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+        FragmentManager fragmentManager = getFragmentManager();
+        Fragment mainFragment = new MainActivityFragment();
 
-
-
+        fragmentManager.beginTransaction()
+                .add(R.id.fragmentHolder, mainFragment)
+                .commit();
 
         Stetho.initializeWithDefaults(this);
     }
